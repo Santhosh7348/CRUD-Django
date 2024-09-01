bf68388cf22ff86cf64097eb059c6635963d79c7
Santhosh7348 <santoshmuttalageri007@gmail.com>
2024-09-02 00:52:06 +0530

    Employee Django

--- project/app/models.py
+++ project/app/models.py
@@ -10,6 +10,7 @@ class Employee(models.Model):
   gender=models.CharField(max_length=25,blank=False,null=False)
 
   
-
+def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
   
